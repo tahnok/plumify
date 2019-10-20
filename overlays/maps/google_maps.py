ce36f4a9785f1fee1e8548637ffe66c9076c2749
Wesley Ellis <tahnok@gmail.com>
2019-10-20 08:39:31 -0400

    calculate appropriate zoom level based on desired real width

--- overlays/maps/google_maps.py
+++ overlays/maps/google_maps.py
@@ -1,14 +1,14 @@
 from urllib.parse import urlencode
 
+import math
 import secrets
 
 class GoogleMaps:
     BASE_URL = 'https://maps.googleapis.com/maps/api/staticmap?'
+    REAL_WITH_KM = 10
+    REAL_HEIGHT_KM = 5
     WIDTH = 514 # width of sample image
     HEIGHT = 257 # height of sample plume image
-    # TODO fix zoom to be accurate to 10km x 5km
-    # https://gis.stackexchange.com/questions/7430/what-ratio-scales-do-google-maps-zoom-levels-correspond-to
-    ZOOM = 15
     MAP_TYPE = 'satellite'
 
     def __init__(self, key=secrets.GOOGLE_API_KEY):
@@ -17,10 +17,29 @@ class GoogleMaps:
     def url(self, latitude, longtitude):
         params = {
             'center': "{},{}".format(latitude, longtitude),
-            'zoom': self.ZOOM,
+            'zoom': self.zoom(self.WIDTH, self.REAL_WITH_KM, latitude),
             'size': "{}x{}".format(self.WIDTH, self.HEIGHT),
             'maptype': self.MAP_TYPE,
             'key': self.key,
         }
 
         return self.BASE_URL + urlencode(params)
+
+    def zoom(self, image_width, real_width_km, latitude):
+        """
+        Calculate the appropriate zoom level for a given latitude according to:
+        https://gis.stackexchange.com/questions/7430/what-ratio-scales-do-google-maps-zoom-levels-correspond-to
+
+        image_width is in pixels
+
+        real_width_km is in km
+
+        latitiude is a floating point number
+
+        metersPerPx = 156543.03392 * Math.cos(latLng.lat() * Math.PI / 180) / Math.pow(2, zoom)
+        """
+        meters_per_pixel = (real_width_km * 1000.0) / image_width 
+
+        zoom = math.log2((156543.0339 * math.cos(latitude * math.pi / 180)) / meters_per_pixel)
+
+        return int(round(zoom))
--- overlays/maps/test_google_maps.py
+++ overlays/maps/test_google_maps.py
@@ -7,7 +7,7 @@ class TestGoogleMaps(unittest.TestCase):
     def test_generate_url(self):
         maps = GoogleMaps(key="test")
         generated_url = urlparse(maps.url(45.0, 20.0))
-        expected_url = urlparse("https://maps.googleapis.com/maps/api/staticmap?zoom=15&size=514x257&maptype=satellite&center=45.0%2C20.0&key=test")
+        expected_url = urlparse("https://maps.googleapis.com/maps/api/staticmap?zoom=12&size=514x257&maptype=satellite&center=45.0%2C20.0&key=test")
         self.assertEquals(expected_url.hostname, generated_url.hostname)
         self.assertEquals(expected_url.path, generated_url.path)
         self.assertEquals(parse_qs(expected_url.query), parse_qs(generated_url.query))
