e6497ff358eea19e5c63589cc4cad5aec4922125
Wesley Ellis <tahnok@gmail.com>
2019-10-19 20:54:29 -0400

    Make pretty with bootstrap and CSS

--- plumify/settings.py
+++ plumify/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,5 @@ STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
